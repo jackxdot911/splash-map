8c9d76596dfb4647f103bb6c9a6517f9dcdbe310
Nishan Kumar <nishan.kumar@7edge.com>
2024-10-28 12:56:44 +0530

    asasas

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,59 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View, ActivityIndicator } from "react-native";
+import awsconfig from './aws-exports'; 
+import React, { useEffect, useState } from 'react';
 import { router } from "expo-router";
+import AuthScreen from "./authScreen";
+import { Amplify } from 'aws-amplify';
+import Auth from '@aws-amplify/auth';
+
+Amplify.configure(awsconfig);
 
 const Index = () => {
-  return (
-    <View style={styles.constainer}>
-      <TouchableOpacity onPress={()=>router.push('/map')}>
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Initialize to false
+  const [loading, setLoading] = useState(true); // Add loading state
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      setLoading(true); // Start loading
+      try {
+        console.log("Checking authentication status...");
+        await Auth.currentAuthenticatedUser();
+        console.log("User is authenticated");
+        setIsAuthenticated(true);
+      } catch (error) {
+        console.error("Authentication error:", error);
+        setIsAuthenticated(false);
+      } finally {
+        setLoading(false); // End loading
+      }
+    };
+    checkAuthStatus();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />; // Loading indicator
+  }
+
+  return isAuthenticated ? (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.push('/map')}>
         <Text style={styles.text}>Map</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={()=>router.push('/scrollAni')}>
+      <TouchableOpacity onPress={() => router.push('/scrollAni')}>
         <Text style={styles.text}>Scroll-Animation</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={()=>router.push('/bottomSheet')}>
+      <TouchableOpacity onPress={() => router.push('/bottomSheet')}>
         <Text style={styles.text}>Bottom sheet</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={()=>router.push('/auth')}>
+      <TouchableOpacity onPress={() => router.push('/auth')}>
         <Text style={styles.text}>Auth</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={()=>router.push('/_sitemap')}>
+      <TouchableOpacity onPress={() => router.push('/_sitemap')}>
         <Text style={styles.text}>Site Map</Text>
       </TouchableOpacity>
     </View>
+  ) : (
+    <AuthScreen setAuth={setIsAuthenticated} />
   );
 };
 
@@ -29,11 +62,11 @@ export default Index;
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
-    fontWeight : 'bold',
+    fontWeight: 'bold',
   },
-  constainer : {
-    flex : 1,
-    justifyContent : 'center',
-    alignItems : 'center'
-  }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
